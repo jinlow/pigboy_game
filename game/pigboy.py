1e4dcea1ee033bb5adab98072582876417b5af06
jinlow <james.d.inlow@gmail.com>
2019-08-04 11:44:09 -0500

    Working more on collision

--- game/pigboy.py
+++ game/pigboy.py
@@ -21,34 +21,46 @@ class Pigboy(pyglet.sprite.Sprite):
         self.jumping = False
         self.jump_force = 0
         self.collision_id = None
+        self.side_collision = False
 
     def update(self, dt, plat):
         print(self.fall_init)
         plat_collision = self.collide(plat)
         # self.fly(dt)
+        self.jump(dt)
         if self.key_handler[key.LSHIFT]:
             self.walking(dt, plat_collision, plat, walk_speed=self.walk_speed*self.run_speed)
         else:
             self.walking(dt, plat_collision, plat, walk_speed=self.walk_speed)
-        self.jump(dt)
+        if plat_collision:
+            self.collision_id = plat.platform_id
         self.gravity(dt, plat_collision, plat)
 
     def walking(self, dt, plat_collision, plat, walk_speed):
         """Allow pig to walk, and control animation"""
-        if (self.y - self.height //2) < plat.ytop and plat_collision:
-            walk_speed = 0
+        # If a new collision happens, stop walking
         if self.key_handler[key.RIGHT]:
+            if (plat_collision and (self.collision_id != plat.platform_id)
+            and (self.collision_id is not None)):
+                self.x -= (walk_speed + 2)
+                walk_speed = 0
+                self.side_collision = True
             self.x += walk_speed
             if self.image != resources.pigboy_animationR:
                 self.image = resources.pigboy_animationR
+                self.side_collision = False
             self.facing_right = True
 
         if self.key_handler[key.LEFT]:
-            # if 0 in plat_collide_list and not self.facing_right:
-            #     walk_speed = 0
+            if (plat_collision and (self.collision_id != plat.platform_id)
+            and (self.collision_id is not None)):
+                self.x += (walk_speed + 2)
+                walk_speed = 0
+                self.side_collision = True
             self.x -= walk_speed
             if self.image != resources.pigboy_animationL:
                 self.image = resources.pigboy_animationL
+                self.side_collision = False
             self.facing_right = False
 
         if not self.key_handler[key.RIGHT] | self.key_handler[key.LEFT]:
@@ -70,6 +82,7 @@ class Pigboy(pyglet.sprite.Sprite):
         and not self.jumping):
             self.jumping = True
             self.jump_force = 10
+            self.side_collision = False
 
         if self.jump_force > 0:
             self.y += self.jump_force
@@ -81,14 +94,14 @@ class Pigboy(pyglet.sprite.Sprite):
 
     def gravity(self, dt, plat_collision, plat):
         """Enact gravity on the pig."""
-        if plat_collision:
-            self.fall_init = 0
-            self.y = plat.ytop + (self.height // 2)
-            self.falling = False
-            self.collision_id = plat.platform_id
-        else:
-            self.y -= self.fall_init
-            self.fall_init += 0.1
+        if not self.side_collision:
+            if plat_collision:
+                self.fall_init = 0
+                self.y = plat.ytop + (self.height // 2)
+                self.falling = False
+            else:
+                self.y -= self.fall_init
+                self.fall_init += 0.1
 
     def collide(self, plat):
         """Check for collision and handle"""
